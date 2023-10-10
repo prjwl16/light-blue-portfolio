28ad453d72f75821b99ab67ca23f83aaf7b2e6b5
prjwl16 <prajumore76@gmail.com>
2023-10-10 23:46:04 +0530

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,27 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      blue: "#345FF6",
+      gunmetal:"#253347",
+      darkElectricBlue:"#5E6E85",
+    },
+    borderColor: {
+      grey:"#D8E2E7",
+    },
+    fontSize: {
+      xl: "64px",
+      l: "48px",
+      m: "24px",
+      s: "20px",
+      bold: "16px"
+    },
+    lineHeight: {
+      default: "70px"
+    },
+    fontFamily:{
+      inter: ['Inter', 'sans-serif']
+    }
   },
   plugins: [],
 }
